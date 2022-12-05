9361bb5b01934e0f990a50a8b81d264dfb072e7c
Robert Onstott <roonstott@gmail.com>
2022-12-05 14:40:22 -0800

    bug fix, working pingpong project
    Co-authored-by: Aitana Shough <aeshough@gmail.com>

--- PingPong.cs
+++ PingPong.cs
@@ -1,31 +1,31 @@
 using System;
 using System.Collections.Generic;
 
-namespace PingPong.Models 
+namespace Models 
 {
   public class PingPong
   {
     public static string MakeList(int number) 
     {
-      List<string> pingPong = new List<string> {};
-      for(int index = 0; index < number; index ++)
+      List<string> pingPong = new List<string>(0);
+      for(int index = 0; index < number; index++)
       {
         int currentNumber = index + 1;
         if (currentNumber % 3 == 0 && currentNumber % 5 == 0) 
         {
-          pingPong[index] = "ping-pong";
+          pingPong.Add("ping-pong");
         }
         else if (currentNumber % 5 == 0) 
         {
-          pingPong[index] = "pong";
+          pingPong.Add("pong");
         }
         else if (currentNumber % 3 == 0) 
         {
-          pingPong[index] = "ping";
+          pingPong.Add("ping");
         }
         else 
         {
-          pingPong[index] = currentNumber.ToString();
+          pingPong.Add(currentNumber.ToString());
         }
       }
       string stringPingPong = string.Join( ",", pingPong);
